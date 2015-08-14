e147e058d08d586de62bf8b37d3e12f83ef26bdc
peb7268 <peb7268@gmail.com>
2015-08-13 22:53:45 -0400

    bulk school uploader added

--- domains/foodfinderga.org/html/application/controllers/import.php
+++ domains/foodfinderga.org/html/application/controllers/import.php
@@ -30,7 +30,28 @@ class Import extends CI_Controller {
 		
 		if($this->uploadProvidersToDB($data)) die('Providers successfully uploaded.');
 		die('Oops there was an error uploading the CSV.'); 
-	}	
+	}
+
+	public function schools(){
+		$this->load->view('import-schools', array('error' => ' '));
+	}
+
+	public function importSchools(){
+		$data 			= ($_FILES['import_csv']);
+		$temp_filename 	= $data['tmp_name'];
+		$file_resource 	= fopen ($temp_filename , 'r');
+		$table 			= [];
+
+		while(($row 	= fgetcsv($file_resource)) !== false){
+			$table[]    = $row;
+		}
+		$properties 	= $table[0];
+
+		$data 			= $this->formatSchoolObjects($properties, $table);
+		
+		if($this->uploadSchoolsToDB($data)) die('Schools successfully uploaded.');
+		die('Oops there was an error uploading the CSV.'); 
+	}
 
 	public function uploadProvidersToDB($data){
 		$success = false;
@@ -63,7 +84,62 @@ class Import extends CI_Controller {
 		return $success;
 	}
 
+	public function uploadSchoolsToDB($data){
+		$success = false;
+		
+		foreach($data as $row):
+			//$this->dd($row);
+		 	$SCHOOL_ID 		= $row->SCHOOL_ID;
+			$SCHOOL_NAME 	= $row->SCHOOL_NAME;
+		 	$SCHOOL_ADDRESS = $row->SCHOOL_ADDRESS;
+		 	$SCHOOL_CITY 	= $row->SCHOOL_CITY;
+		 	$STATE 			= $row->STATE;
+		 	$COUNTY 		= $row->COUNTY;
+			$SCHOOL_ZIP 	= $row->SCHOOL_ZIP;
+		 	$SCHOOL_PHONE 	= $row->SCHOOL_PHONE;
+
+			$sql = "INSERT INTO ff_school (school_name, school_address1, city, state, County, zipcode, phonenumber) 
+			VALUES ("
+			.$this->db->escape($SCHOOL_NAME).","
+			.$this->db->escape($SCHOOL_ADDRESS).","
+			.$this->db->escape($SCHOOL_CITY).","
+			.$this->db->escape($STATE).","
+			.$this->db->escape($COUNTY).","
+			.$this->db->escape($SCHOOL_ZIP).","
+			.$this->db->escape($SCHOOL_PHONE).")";
+
+			$this->db->query($sql);
+		endforeach;
+
+		if($this->db->affected_rows() > 0) $success = TRUE;
+		return $success;
+	}
+
 	public function formatProviderObjects($properties, $table){
+		$data 	= [];
+		$count 	= count($properties);
+
+		foreach($table as $idx => $row):
+			if(count($row) !== $count) die('error with the csv file on row: '.  ($idx + 1));
+
+			$provider = [];
+			$provider['SCHOOL_ID'] 		= $row[0];
+			$provider['SCHOOL_NAME'] 	= $row[1];
+			$provider['SCHOOL_ADDRESS']	= $row[2];
+			$provider['SCHOOL_CITY'] 	= $row[3];
+			$provider['STATE'] 			= $row[4];
+			$provider['COUNTY'] 		= $row[5];
+			$provider['SCHOOL_ZIP'] 	= $row[6];
+			$provider['SCHOOL_PHONE'] 	= $row[7];
+
+			$data[] = (object) $provider;
+		endforeach;
+
+		return $data;
+	}
+
+
+	public function formatSchoolObjects($properties, $table){
 		$data 	= [];
 		$count 	= count($properties);
 
