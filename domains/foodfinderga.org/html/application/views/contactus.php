b4bdeca8e7b0562d60ff5fb4d68618145f0e52c6
Alex Nachlas <alex.nachlas@stablekernel.com>
2015-12-10 12:04:16 -0500

    Updated contact us page

--- domains/foodfinderga.org/html/application/views/contactus.php
+++ domains/foodfinderga.org/html/application/views/contactus.php
@@ -15,7 +15,9 @@
                             src="<?php echo base_url(); ?>img/front/homeicon.png"/></a></div>
                 <div class="termpad_wrap contactus_wrap">
                     <h2 style="text-align: center">Contact Us</h2>
-                    <h4 style="text-align: center"><a href="mailto:info@foodfinderga.org">info@foodfinderga.org</a></h4>
+                    <h4 style="text-align: center">Executive Director:  Lynn Griffin</h4>
+                    <h4 style="text-align: center">Phone: 770-527-2181</h4>
+                    <h4 style="text-align: center">Email: <a href="mailto:info@foodfinderga.org">info@foodfinderga.org</a></h4>
                 </div>
                 <?php include('footer_menu.php'); ?>
             </div>
